139d0f04584fc39c001b99830b5cacc984cb70a1
Rolando José Garcia Escobar <rolaescobar@gmail.com>
2026-02-14 11:59:55 -0600

    se agrego detalle del algoritmo

---
+++ alg-0.py
@@ -0,0 +1,23 @@
+# Algoritmo para calcular el volumen de una caja
+
+# Leer las medidas A, B y C
+A = float(input("Ingrese la medida A: ")) 
+B = float(input("Ingrese la medida B: "))
+C = float(input("Ingrese la medida C: "))
+
+# Calcular el volumen
+volumen = A * B * C
+
+
+# Mostrar el resultado
+print("El volumen de la caja es:", volumen,"cm")
+
+#Pseudo código para calcular el volumen de una caja:
+# Algoritmo para calcular el volumen de una caja:
+# Inicio
+#   Leer A
+#   Leer B
+#   Leer C
+#   Calcular volumen = A * B * C2
+#   Mostrar volumen
+# Fin
