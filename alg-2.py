34680b94871003c7c4a27d50b629270084b3c7cd
Rolando José Garcia Escobar <rolaescobar@gmail.com>
2026-02-14 12:16:32 -0600

    Como te llamas

---
+++ alg-2.py
@@ -0,0 +1,11 @@
+
+Nombre = str(input("Como te llamas? ")) 
+
+print("Hola", Nombre, "bienvenido a la clase de Introduccion a la Programacion")
+Edad = int(input("Que edad tienes? "))
+
+if Edad < 18:
+    print("Eres menor de edad") 
+else:
+    print("Eres mayor de edad")
+
