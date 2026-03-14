b82da360b722ddb50145e890419f89c280e69478
Rolando José Garcia Escobar <rolaescobar@gmail.com>
2026-03-14 10:39:20 -0600

    Lista de modificación

---
+++ list-02.py
@@ -0,0 +1,7 @@
+frutas  = ["manzana", "pera", "naranja", "uva" ]
+
+print("Versión inicial de la lista:")
+print(frutas)
+frutas[1] = "fresa"
+print("Versión modificada de la lista:")
+print(frutas)
